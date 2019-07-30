5239f4e7cc7a0ac6d402314853693c5130765288
CMaC253 <colin_Mcdonald2@msn.com>
2019-07-30 14:25:27 -0700

    Add project files.

---
+++ LAP3/MatchNumForm.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MatchNumForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.resultTextBox = New System.Windows.Forms.TextBox()
+        Me.playButton = New System.Windows.Forms.Button()
+        Me.paymentPlanTextBox = New System.Windows.Forms.TextBox()
+        Me.luckyNumberTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'resultTextBox
+        '
+        Me.resultTextBox.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.resultTextBox.Location = New System.Drawing.Point(8, 85)
+        Me.resultTextBox.Multiline = True
+        Me.resultTextBox.Name = "resultTextBox"
+        Me.resultTextBox.ReadOnly = True
+        Me.resultTextBox.Size = New System.Drawing.Size(215, 74)
+        Me.resultTextBox.TabIndex = 7
+        Me.resultTextBox.TabStop = False
+        '
+        'playButton
+        '
+        Me.playButton.Location = New System.Drawing.Point(229, 85)
+        Me.playButton.Name = "playButton"
+        Me.playButton.Size = New System.Drawing.Size(44, 27)
+        Me.playButton.TabIndex = 8
+        Me.playButton.Text = "Play!"
+        Me.playButton.UseVisualStyleBackColor = True
+        '
+        'paymentPlanTextBox
+        '
+        Me.paymentPlanTextBox.Location = New System.Drawing.Point(82, 40)
+        Me.paymentPlanTextBox.Name = "paymentPlanTextBox"
+        Me.paymentPlanTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.paymentPlanTextBox.TabIndex = 4
+        '
+        'luckyNumberTextBox
+        '
+        Me.luckyNumberTextBox.Location = New System.Drawing.Point(82, 6)
+        Me.luckyNumberTextBox.Name = "luckyNumberTextBox"
+        Me.luckyNumberTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.luckyNumberTextBox.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(184, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 28)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Between 1 and 3"
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(6, 40)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(92, 18)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Payment Plan"
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(187, 37)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 29)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = """Lump Sum"" or ""20-year Spread"""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Lucky Number"
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(6, 70)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 18)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Result:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 165)
+        Me.Controls.Add(Me.resultTextBox)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.playButton)
+        Me.Controls.Add(Me.paymentPlanTextBox)
+        Me.Controls.Add(Me.luckyNumberTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Match the Number!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents resultTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents playButton As System.Windows.Forms.Button
+    Friend WithEvents paymentPlanTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents luckyNumberTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ LAP3/MatchNumForm.vb
@@ -0,0 +1,103 @@
+﻿'MIS 322
+'Colin McDonald 
+'LAP 3
+'04/30/19
+
+
+Option Explicit On : Option Strict On
+Public Class MatchNumForm
+    Private Sub playButton_Click(sender As Object,
+                e As EventArgs) Handles playButton.Click
+        'Handle PlayButton Actions
+
+        Dim rng As New Random()
+        Dim luckyNum, randomNum As Integer
+        Dim winnings, paymentAmount, taxRate As Double
+        Dim paymentPlan As String
+
+        'Handle error for luckyNum conversion and check whether
+        'it is in the 1-3 range
+        Try
+            luckyNum = Convert.ToInt32(luckyNumberTextBox.Text)
+            If luckyNum < 1 Or luckyNum > 3 Then
+                MessageBox.Show("The player number must be between " &
+                                "1 and 3 (inclusive)", "Number Range Error")
+                Exit Sub
+            End If
+        Catch fe As FormatException
+            MessageBox.Show("Pleae enter a whole integer",
+                             "Number Format Error")
+            Exit Sub
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Unexpected Error")
+            Exit Sub
+        End Try
+
+        'Check for an empty box in payment plan
+        If paymentPlanTextBox.Text = "" Then
+            MessageBox.Show("Please enter a payment plan", "Text Box Fulfillment Error")
+            Exit Sub
+        End If
+
+        'Assign the paymentPlan Text Box to the paymentPlan variable
+        paymentPlan = paymentPlanTextBox.Text.ToUpper
+
+        'Here we are going to check if payment plan is either of the 
+        'options available, if not then we will throw an error.
+        If Not paymentPlan = "LUMP SUM" And
+           Not paymentPlan = "20-YEAR SPREAD" Then
+            MessageBox.Show("The payment plan must be either Lump Sum " &
+                            "or 20-year Spread", "Payment Plan Selection Error")
+            Exit Sub
+        End If
+
+        'Assign Random numbers 1-3 and 1-2,000,000 to the appropriate variables
+        randomNum = rng.Next(1, 4)
+        winnings = rng.Next(1, 2000001)
+
+        'Here's where the magic happens, if randomNum and luckyNum are not equal
+        'Show the losers message
+        If Not randomNum = luckyNum Then
+            resultTextBox.Text =
+                "The random number was " & randomNum & vbNewLine
+            resultTextBox.Text &=
+                "You Lost your bet."
+        Else
+            'Check if lump sum or 20 year spread
+            If paymentPlan = "LUMP SUM" Then
+                paymentAmount = winnings / 2
+            Else
+                paymentAmount = winnings / 20
+            End If
+
+            'check which tax bracket we are in
+            If paymentAmount > 500000 Then
+                taxRate = 0.4
+            ElseIf paymentAmount > 300000 Then
+                taxRate = 0.3
+            ElseIf paymentAmount > 100000 Then
+                taxRate = 0.25
+            ElseIf paymentAmount > 10000 Then
+                taxRate = 0.1
+            Else
+                taxRate = 0
+            End If
+
+            'post results to the result text box
+            resultTextBox.Text =
+               "The random number was " & randomNum & " You WON!" & vbNewLine
+            resultTextBox.Text &=
+                "Your winnings are " & winnings.ToString("C2") & vbNewLine
+            resultTextBox.Text &=
+               "Your payment amount is " & paymentPlan.ToString() & vbNewLine
+            resultTextBox.Text &=
+                "Your payment amount is " & paymentAmount.ToString("C2") & vbNewLine
+            resultTextBox.Text &=
+                "Your tax rate is " & taxRate.ToString("P2")
+
+
+        End If
+
+
+    End Sub '------------------------------------------------------------------------------
+End Class
